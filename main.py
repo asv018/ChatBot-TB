2bb3f0e2091455588b542fa815c109a634b1bf02
Arpit Singh Verma <21bcs089@nith.ac.in>
2023-07-30 16:31:18 +0530

    System Prompt

--- main.py
+++ main.py
@@ -5,13 +5,12 @@ import os
 from typing import List
 
 # Load your OpenAI API key
-models.OpenAI.api_key = "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"
+models.OpenAI.api_key = "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"
 # or from environment variable:
 # models.OpenAI.api_key = os.getenv("OPENAI_API_KEY")
 
-# Initial prompt for GPT-3.5 Turbo to start the conversation
-INITIAL_PROMPT = """Hello! I am your mental health support chatbot. How are you feeling today? Please share your thoughts and emotions with me, and I'll do my best to provide guidance and support.
-"""
+# Prompt for GPT-3.5 Turbo
+SYSTEM_PROMPT = """You are a career counselor speaking with a client who is seeking advice on their career path."""
 
 @textbase.chatbot("talking-bot")
 def on_message(message_history: List[Message], state: dict = None):
@@ -27,20 +26,11 @@ def on_message(message_history: List[Message], state: dict = None):
     else:
         state["counter"] += 1
 
-    # If this is the first interaction, use the initial prompt
-    if len(message_history) == 0:
-        bot_response = INITIAL_PROMPT
-    else:
-        # Generate GPT-3.5 Turbo response based on the user's message
-        response_object = models.OpenAI.generate(
-            system_prompt=message_history[-1].content,  # Use the last user message as the prompt
-            message_history=message_history,
-            model="gpt-3.5-turbo",
-        )
-        # Extract the actual text from the response object
-        bot_response = response_object['choices'][0]['message']['content']
-
-    # Add a question at the end of the response
-    bot_response += "\n\nHow can I assist you further?"
+    # # Generate GPT-3.5 Turbo response
+    bot_response = models.OpenAI.generate(
+        system_prompt=SYSTEM_PROMPT,
+        message_history=message_history,
+        model="gpt-3.5-turbo",
+    )
 
     return bot_response, state
